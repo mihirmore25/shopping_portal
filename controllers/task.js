3ecbb80a2f06b67e9b34547a0b073af6d5450c7a
Mihir More <mihirmore.25@gmail.com>
2024-03-28 17:07:49 +0530

    Add update task API endpoint

--- controllers/task.js
+++ controllers/task.js
@@ -100,3 +100,43 @@ export const deleteTask = async (req, res) => {
         message: "Task has been deleted successfully.",
     });
 };
+
+export const updateTask = async (req, res) => {
+    const { title, description, status } = req.body;
+
+    let taskId = req.params.id;
+
+    if (!taskId || String(taskId).length < 24) {
+        return res.status(404).json({
+            status: false,
+            message: "Please search task with valid task id.",
+        });
+    }
+
+    const task = await Task.findById(taskId).select("-__v");
+
+    if (taskId && (task === null || undefined || 0)) {
+        return res.status(404).json({
+            status: false,
+            message: `Task did not found with ${taskId} id.`,
+        });
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(
+        taskId,
+        {
+            title,
+            description,
+            status,
+        },
+        { new: true }
+    ).select("-__v");
+
+    console.log("Updated Task --> ", updatedTask);
+
+    return res.status(200).json({
+        status: true,
+        data: updatedTask,
+        message: "Task has been updated successfully.",
+    });
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { createTask, deleteTask, getAllTasks, getTask } from "../controllers/task.js";
+import {
+    updateTask,
+    createTask,
+    deleteTask,
+    getAllTasks,
+    getTask,
+} from "../controllers/task.js";
 
 const router = express.Router();
 
@@ -7,5 +13,6 @@ router.post("/", createTask);
 router.get("/", getAllTasks);
 router.get("/:id", getTask);
 router.delete("/:id", deleteTask);
+router.put("/:id", updateTask);
 
 export default router;
