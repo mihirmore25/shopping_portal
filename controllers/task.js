0bf4d4657a215e2141357939f890fb56090d38ea
Mihir More <mihirmore.25@gmail.com>
2024-03-28 15:40:12 +0530

    Add create task API endpoint

--- controllers/task.js
+++ controllers/task.js
@@ -2,4 +2,29 @@ import { Task } from "../models/Task.js";
 
 export const createTask = async (req, res) => {
     console.log(`Create Task!`);
+
+    let { title, description } = req.body;
+
+    if (!title || !description) {
+        return res.status(500).json({
+            status: false,
+            error: res.statusCode,
+            message: "Both Title and Description are required!",
+        });
+    }
+
+    const newTask = await Task.create({
+        title,
+        description,
+    });
+
+    const newCreatedTask = await newTask.save();
+
+    console.log(newCreatedTask._doc);
+
+    return res.status(201).json({
+        status: true,
+        data: [newCreatedTask._doc],
+        message: "New Task created successfully.",
+    });
 };
