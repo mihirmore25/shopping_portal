ed37e643d4ea637efae5031f482e64a096e9707d
Mihir More <mihirmore.25@gmail.com>
2024-04-04 13:59:28 +0530

    Add CORS options

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import morgan from "morgan";
 import express from "express";
+import cors from "cors";
 import taskRoutes from "./routes/tasks.js";
 import { dbClient } from "./db/db.js";
 
@@ -9,6 +10,14 @@ const app = express();
 
 // DB connection
 dbClient();
+
+const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
